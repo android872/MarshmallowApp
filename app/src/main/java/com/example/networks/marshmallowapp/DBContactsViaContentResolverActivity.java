0e199d74188effc439fa443ab695e27f88eb4487
android872 <nasir.ali@hotmail.com>
2017-04-18 22:55:33 +0500

    Office 2017-04-18

--- app/src/main/java/com/example/networks/marshmallowapp/DBContactsViaContentResolverActivity.java
+++ app/src/main/java/com/example/networks/marshmallowapp/DBContactsViaContentResolverActivity.java
@@ -1,25 +1,33 @@
 package com.example.networks.marshmallowapp;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DBContactsViaContentResolverActivity extends AppCompatActivity {
 
+    private ListView names;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dbcontacts_via_content_resolver);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        names = (ListView) findViewById(R.id.names);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,15 +35,35 @@ public class DBContactsViaContentResolverActivity extends AppCompatActivity {
                 ContentResolver cr = getContentResolver();
                 Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI,
                         new String[]{ContactsContract.Contacts.DISPLAY_NAME},null,null,null);
-                List<String> contacts = new List<String>();
+                List<String> contacts = new ArrayList<String>();
                 if(cursor.moveToFirst()){
                     do{
                         contacts.add(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
                     }
                     while(cursor.moveToNext());
                 }
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(),
+                        R.layout.contact_detail,R.id.name,contacts);
+                names.setAdapter(adapter);
             }
         });
+
+        final int REQUEST_CODE_ASK_PERMISSIONS =  123;
+        int hasReadContactsPermission =
+                ContextCompat.checkSelfPermission(DBContactsViaContentResolverActivity.this,
+                        Manifest.permission.READ_CONTACTS);
+
+                if(hasReadContactsPermission != PackageManager.PERMISSION_GRANTED){
+                    if(!ActivityCompat.shouldShowRequestPermissionRationale(DBContactsViaContentResolverActivity.this,
+                            Manifest.permission.READ_CONTACTS)){
+                        ActivityCompat.requestPermissions(DBContactsViaContentResolverActivity.this,
+                                new String[]{Manifest.permission.READ_CONTACTS},
+                                REQUEST_CODE_ASK_PERMISSIONS);
+                        return;
+                    }
+                }
+                ActivityCompat.requestPermissions(DBContactsViaContentResolverActivity.this,
+                        new String[] {Manifest.permission.READ_CONTACTS},REQUEST_CODE_ASK_PERMISSIONS);
     }
 
 }
---
+++ app/src/main/java/com/example/networks/marshmallowapp/FriendsProvider.java
@@ -0,0 +1,8 @@
+package com.example.networks.marshmallowapp;
+
+/**
+ * Created by Networks on 4/18/2017.
+ */
+
+public class FriendsProvider {
+}
